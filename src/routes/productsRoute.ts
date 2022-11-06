6d5d17dc45b25d18535903e87b5cd9310ede360e
felipe <fortes.felipe95@gmail.com>
2022-11-06 16:38:11 -0300

    Requisito 06

--- src/routes/productsRoute.ts
+++ src/routes/productsRoute.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
 import ProductController from '../controllers/products.controller';
+import productsMiddlewares from '../middlewares/products.middleware';
 
 const router = Router();
+const { productsNameMiddleware, productsAmountMiddleware } = productsMiddlewares;
 
 const productsController = new ProductController();
-router.post('/', productsController.createProduct.bind(productsController));
+router.post(
+  '/', 
+  productsNameMiddleware,
+  productsAmountMiddleware, 
+  productsController.createProduct.bind(productsController),
+);
 router.get('/', productsController.getAllProduct.bind(productsController));
 
 export default router;
