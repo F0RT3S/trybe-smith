9a6c429f999a7535640054a8bd3939c28ceb8760
felipe <fortes.felipe95@gmail.com>
2022-11-06 17:12:24 -0300

    Requisito 07

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,10 +1,22 @@
 import { Router } from 'express';
 import UserController from '../controllers/user.controller';
+import userMiddleware from '../middlewares/user.middleware';
 
 const router = Router();
+const { usernameMiddleware,
+  classeMiddleware,
+  levelMiddleware,
+  passwordMiddleware } = userMiddleware;
 
 const userController = new UserController();
 
-router.post('/', userController.createUser.bind(userController));
+router.post(
+  '/', 
+  usernameMiddleware,
+  classeMiddleware,
+  levelMiddleware,
+  passwordMiddleware,
+  userController.createUser.bind(userController),
+);
 
 export default router;
